ac6f177db9d0f9b5b64d9366a0766a700c0ffb3c
Kaido Iwamoto <kaido@odiak.net>
2023-07-11 16:26:01 +0900

    Setup Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG ?? '')
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+getAnalytics(app)
+
 createRoot(document.getElementById('app')!).render(<App />)
